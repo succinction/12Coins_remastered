f49b9c3173a7f55bedb6e8bced392e901c6abb97
Joe Howard <succinction@gmail.com>
2017-06-13 17:26:21 -0700

    gm

--- src/App.js
+++ src/App.js
@@ -9,31 +9,26 @@ import ScratchPad   from './components/ScratchPad';
 import Scale from './components/Scale'
 import Timer   from './components/Timer';
 import {TweenMax, Power3}  from "gsap";
-// import {TweenLite, Power3}  from "gsap";
-// import {TweenLite, Elastic, TweenMax, Power3}  from "gsap";
+
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-
-        const initial_game = 1;
-
-
-        const initialNumberOfCoins = 12;
+        this.gameNumber = 1;
+        const initialNumberOfCoins = 9;
+        this.numberOfCoins = initialNumberOfCoins
         this.lucky_number = Math.floor(Math.random() * initialNumberOfCoins);
         this.coin_weights = [Number(6), Number(5), Number(7)];
         this.measurementsUsed = 0;
         this.readout = "Find the false coin within three measurements on the scale.";
         this.light_or_heavy = Math.floor(Math.random() * 2) + 1
-
-
-        this.version = "2.5.0";
+        this.version = "2.5.1-2017May";
         this.state = {
-            gameNumber: initial_game,
+            gameNumber: this.gameNumber,
             labels: true,
             msg: this.readout,
-            numberOfCoins: initialNumberOfCoins,
+            numberOfCoins: this.numberOfCoins,
             balanced: 0
         };
 
@@ -41,27 +36,56 @@ class App extends Component {
             return false;
         };
         document.body.setAttribute('unselectable', 'on', 0);
+
+        this.userName = "guest"
+
+
+        this.renew_game_object = () => {
+
+
+
+            let sign = (this.light_or_heavy > 1) ? "+" : "-";
+            return {
+            userName: this.userName,
+            gameNumber: this.gameNumber,
+            gameType: this.numberOfCoins,
+            falseCoin: this.lucky_number + sign,
+            finalScore: "undetermined",
+            measurements: []
+        }
+        };
+
+
         this.reset_location_array = (num) => {
             let numb = (num > 0) ? num : this.state.numberOfCoins;
-            // console.log("entered reset_location.. numb :   : ", numb)
-
-            let arr = [0, 0]
+            let arr = [0, 0];
             for (let i = 0; i < numb; i++) {
                 arr.push(0);
             }
             return arr
-
-
         };
 
+
+
+
+
         this.coin_location_array = this.reset_location_array();
-
         this.coin_locations = this.coin_location_array.toString();
-
         // COLOR WARP
         this.colr = {h: 0, s: 50, l: 100};
         this.element = document.getElementsByTagName("body")[0];
         // /COLOR WARP
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+        // init_gameObject
+        // this.gameObject = {}
+
+
+        this.gameObject = this.renew_game_object();
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 
     }
@@ -69,46 +93,92 @@ class App extends Component {
     // COLOR WARP
     applyColor = () => {
         this.element.style.backgroundColor = "hsl(" + this.colr.h + "," + this.colr.s + "%," + this.colr.l + "%)";
-    }
+    };
     end_color = () => {
         TweenMax.to(this.colr, 20, {h: 0, l: 100, onUpdate: this.applyColor});
-    }
+    };
     // /COLOR WARP
 
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+    updateGameObject = () => {
+
+        // let sign = (this.light_or_heavy > 1) ? "+" : "-";
+        // this.userName = "guest"
+
+        // init_gameObject
+        // this.gameObject = {
+        //     userName: this.userName,
+        //     gameNumber: this.state.gameNumber,
+        //     gameType: this.state.numberOfCoins,
+        //     falseCoin: this.lucky_number + sign
+        // }
+
+
+        //
+        let measuretime = this._child_timer.get_time();
+        let ankh_xy = [parseInt( document.getElementById("ankh")._gsTransform.x, 10 ) , parseInt(  document.getElementById("ankh")._gsTransform.y, 10)]  ;
+        let feather_xy = [parseInt( document.getElementById("feather")._gsTransform.x, 10 ) , parseInt(  document.getElementById("feather")._gsTransform.y, 10)] ;
+//        let measures = [ankh_xy, feather_xy];
+
+
+        let push_position = { "time": measuretime, "ankh": ankh_xy, "feather": feather_xy };
+
+
+        for (let i = this.state.numberOfCoins - 1; i >= 0; i--) {
+            const this_coin = document.getElementById("coin" + i);
+            const coin_xy = [ parseInt(  this_coin._gsTransform.x, 10 ) , parseInt( this_coin._gsTransform.y, 10)];
+            // push_position.push(coin_xy);
+            // Object.assign(push_position, )
+            push_position["coin" + i] = coin_xy;
+        }
+
+        // Object.assign(this.gameObject.measurements, push_position)
+         this.gameObject.measurements.push(push_position)
+        console.log(this.gameObject)
+        // console.log(this.gameObject.toString())
+        console.log(JSON.stringify(this.gameObject)   )
+    };
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
     reset_game = (numbr) => {
-
-
         let lucky_number_init = -1;
         this._child_timer.reset_time();
         if (typeof(numbr) === "number") {
             lucky_number_init = Math.floor(Math.random() * numbr);
         } else {
-
             lucky_number_init = Math.floor(Math.random() * this.state.numberOfCoins);
         }
         this.measurementsUsed = 0;
         this.readout = "Find the false coin within three measurements on the scale.";
         this.lucky_number = lucky_number_init;
         this.light_or_heavy = Math.floor(Math.random() * 2) + 1;
+        this.gameNumber++;
         this.setState({
-            gameNumber: this.state.gameNumber + 1,
+            gameNumber: this.gameNumber,
             msg: this.readout,
         });
         let icons = ["#scale_icon0", "#scale_icon1", "#scale_icon2"]
         TweenMax.to(icons, .5, {autoAlpha: 1, ease: Power3.easeOut});
         TweenMax.to(["#cheat_btn"], 2, {color: "hsl(0, 0%, 100%)"})
         TweenMax.to(["#messenger"], 2, {color: "hsl(0, 0%, 0%)"})
-
         this.reset_coins(numbr);
 
+        this.gameObject = this.renew_game_object();
 
     };
-
     replace_coins = () => {
         this._child.replace_coins()
         this.reset_coins()
-    }
-
+    };
     reset_coins = (num) => {
 
         this.readout = this.coin_locations;
@@ -119,52 +189,35 @@ class App extends Component {
             msg: this.readout,
             balanced: 0
         });
-
     }
-
-
     /////////////////////////////////////////////////////////////////////////
-
-
     balance_scale = (tc) => {
-        // console.log("balance_scale << << ARG:tc =  ", tc)
-        // console.log(tc)
-        // console.log('tc.changedTouches ', tc.changedTouches)
-        // console.log(tc.changedTouches[0])
-        // console.log(tc.changedTouches[0].clientY)
         let balance_mod = 0;
         if (tc.clientY < 428) {
             let offset = window.innerWidth / 2;
-            //console.log("offset: " + offset )
-
             balance_mod = (tc.clientX < offset) ? 1 : -1;
         } else if (tc.changedTouches && tc.changedTouches[0].clientY < ( (window.screen.height > 428) ? 428 : window.screen.height - 100)) {
-
             let offset = window.screen.width / 2;
-            //console.log("changedTouches  <  offset : " + offset , " y " + tc.changedTouches[0].clientY)
             balance_mod = (tc.changedTouches[0].clientX < offset) ? 1 : -1;
         } else {
             balance_mod = 0;
         }
         let balanced = 0;
-
         let indexx = Number(tc.target.id.substr(2));
-        // console.log("indexx : " + indexx + "  :  ");
-        // console.log(tc)
-        // console.log(tc.clientX, tc.clientY)
-        // console.log(tc.target.id)
         this.coin_location_array[indexx] = balance_mod;
-
-
         let coins_on_scale_now = 0;
         let measurement_constituted = 0;
         for (let i = 0; i < this.coin_location_array.length; i++) {
             coins_on_scale_now += Math.abs(this.coin_location_array[i]);
             measurement_constituted += this.coin_location_array[i];
         }
-
-
         if (measurement_constituted === 0) {
+
+            /////
+
+
+
+
             // show lucky
             for (let i = 0; i < this.coin_location_array.length; i++) {
                 if (i === this.lucky_number) {
@@ -176,10 +229,9 @@ class App extends Component {
                 } else {
                     balanced += this.coin_location_array[i] * this.coin_weights[0];
                 }
-
             }
         } else {
-            // hide luicky
+            // hide lucky
             for (let i = 0; i < this.coin_location_array.length; i++) {
                 if (i === this.coin_location_array.length - 2) {
                     balanced += this.coin_location_array[i] * this.coin_weights[1];
@@ -190,40 +242,23 @@ class App extends Component {
                 }
             }
         }
-
-
         this.setState({
             balanced: balanced,
             msg: Number(tc.target.id.substr(2)) + " clientX: " + tc.clientX
         });
         this.score(measurement_constituted, coins_on_scale_now)
     };
-
-
     score = (measurement_constituted, coins_on_scale_now) => {
-
         let time = this._child_timer.get_time();
-
         let number_of_coins = this.state.numberOfCoins;
-        // console.log('number_of_coins', number_of_coins)
-
         let coin_locations_now = this.coin_location_array.toString();
         //
-        // let coins_on_scale_now = 0;
-        // let measurement_constituted = 0;
-        // for (let i = 0; i < this.coin_location_array.length; i++) {
-        //     coins_on_scale_now += Math.abs(this.coin_location_array[i]);
-        //     measurement_constituted += this.coin_location_array[i];
-        // }
-        //
-        //
-
-
-        // console.log('coins_on_scale_now', coins_on_scale_now)
-        // console.log('coin_locations_now', coin_locations_now)
-        // console.log('measurement_constituted', measurement_constituted)
-
         if (measurement_constituted === 0 && coins_on_scale_now > 0 && this.coin_locations !== coin_locations_now) {
+
+
+        this.updateGameObject();
+
+
             if ((Math.abs(this.coin_location_array[number_of_coins]) === 1 || Math.abs(this.coin_location_array[number_of_coins + 1]) === 1) && coins_on_scale_now === 2) {
                 if (this.state.balanced === 0) {
                     if (this.measurementsUsed < 3) {
@@ -269,29 +304,20 @@ class App extends Component {
                 }
             } else {
                 this.measurementsUsed++;
-                // if (this.measurementsUsed === 1) {
-                //     //attempts[number_of_coins]++;
-                // }
                 this.readout = this.measurementsUsed + ' of 3 measurements used.';
 
                 TweenMax.to("#scale_icon" + (this.measurementsUsed - 1), .5, {autoAlpha: 0.2, ease: Power3.easeOut});
 
-                // if (this.measurementsUsed > 5) {
-                // } else if (this.measurementsUsed > 3) {
-                // } else if (this.measurementsUsed === 3) {
-                // }
             }
         }
         this.coin_locations = coin_locations_now;
 
 
+        // this.updateGameObject();
+
         this.setState({
-            // balanced: balancedv,
-            // msg: Number(tc.target.id.substr(2)) + " clientX: " + tc.clientX
             msg: this.readout
         })
-        // this.setState({msg: this.readout});
-        // this.tip_scale();
     };
 
 
@@ -341,7 +367,6 @@ class App extends Component {
         this.reset_game(12)
     };
 
-
     toggle_labels = () => {
         this.setState({
             labels: !this.state.labels
@@ -382,6 +407,8 @@ class App extends Component {
 
 
                 {TRUE && <ScratchPad version={this.version}/> }
+
+
             </div>
 
         );
